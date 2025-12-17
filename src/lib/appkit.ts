eaa76b824e4c11bf9c9900cd5c8a4258fad050e8
Josh <josh_alot@protonmail.com>
2025-12-17 07:41:05 -0300

    reown appkit setup

---
+++ src/lib/appkit.ts
@@ -0,0 +1,42 @@
+import { createAppKit } from '@reown/appkit/react'
+import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
+import { QueryClient } from '@tanstack/react-query'
+import { mainnet } from '@reown/appkit/networks'
+
+// 0. QueryClient (React Query)
+export const queryClient = new QueryClient()
+
+// 1.  (pegue em https://cloud.walletconnect.com → agora é Reown Dashboard)
+export const projectId = 'Sa4dd25a83c5997000716dc185fdf75be' // TODO: Use environment variavles
+
+// 2. Metadados da sua DApp
+const metadata = {
+  name: 'Crypto Portfolio DApp',
+  description: 'Cryptocurrencies Portfolio Dashboard',
+  url: 'http://localhost:5173',     // TODO: Use environment variavles
+  icons: ['https://avatars.githubusercontent.com/u/179229932'], // TODO: create a logo for the Dapp
+}
+
+// 3. Redes suportadas (você pode adicionar mais depois)
+const networks = [mainnet];
+
+// 4. Criar Wagmi Adapter
+export const wagmiAdapter = new WagmiAdapter({
+  networks,
+  projectId,
+  ssr: false,
+})
+
+// 5. Criar o modal da AppKit (uma vez só, fora de componente React)
+createAppKit({
+  adapters: [wagmiAdapter],
+  networks,
+  projectId,
+  metadata,
+  features: {
+    analytics: true,
+  },
+})
+
+// 6. Exportar o config Wagmi para usar no provider
+export const wagmiConfig = wagmiAdapter.wagmiConfig
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { WagmiProvider } from 'wagmi'
+import { queryClient, wagmiConfig } from './lib/appkit'
+import { QueryClientProvider } from '@tanstack/react-query'
 import App from './App.tsx'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </WagmiProvider>
   </StrictMode>,
 )
